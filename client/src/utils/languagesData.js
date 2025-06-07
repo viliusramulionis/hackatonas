5c106eed1a4946922108fdfff554a9447ca34c7c
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2025-06-07 12:29:05 +0100

    update registration and add login

---
+++ client/src/utils/languagesData.js
@@ -0,0 +1,12 @@
+const languagesData = [
+  { label: 'Python', value: 'Python' },
+  { label: 'Javascript', value: 'Javascript' },
+  { label: 'C++', value: 'C++' },
+  { label: 'Java', value: 'Java' },
+  { label: 'Go', value: 'Go' },
+  { label: 'Ruby', value: 'Ruby' },
+  { label: 'SQL', value: 'SQL' },
+  { label: 'Other', value: 'Other' },
+];
+
+export default languagesData;
