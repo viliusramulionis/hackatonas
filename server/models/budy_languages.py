30baa7f54ae45cb7806e542b18d9bd2b66732d3b
AurPutr <aurelija.putramentaite@gmail.com>
2025-06-07 17:07:46 +0300

    budy languages update

--- server/models/budy_languages.py
+++ server/models/budy_languages.py
@@ -9,4 +9,9 @@ class Budy_languages(Database) :
         # Sukuriama naudojama lentelė user
         self.cur.execute("CREATE TABLE IF NOT EXISTS budy_languages (id INTEGER PRIMARY KEY AUTOINCREMENT, language_id INTEGER, budy_id INTEGER, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
 
-
+# Vienos eilutės pridėjimas
+    def insert_row(self, data) :
+        self.cur.execute(
+            f"INSERT INTO budy_languages (language_id, budy_id) VALUES('{data['language_id']}', '{data['budy_id']}')")
+        self.con.commit()
+        return self
