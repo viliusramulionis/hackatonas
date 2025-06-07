22f93f0d982a1bf09ef55d56d957b1277d8131d1
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2025-06-07 18:17:02 +0100

    browserrouter fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,17 +9,19 @@ import { EditProfile } from './pages/EditProfile';
 function App() {
   return (
     <div className="min-h-screen bg-[#111827] text-white">
-      <AuthProvider>
-        <Router>
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/budies/:id" element={<BuddyCardDetailed />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/edit/profile/:id" element={<EditProfile />} />
-          </Routes>
-        </Router>
-      </AuthProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <Router>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/budies/:id" element={<BuddyCardDetailed />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/edit/profile/:id" element={<EditProfile />} />
+            </Routes>
+          </Router>
+        </AuthProvider>
+      </BrowserRouter>
     </div>
   );
 }
