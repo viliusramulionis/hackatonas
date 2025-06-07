aa2aafbb9c67e04931030bb2d3b2a9d1bbc1b472
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2025-06-07 18:25:36 +0100

    add login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,9 @@ import Navigation from './components/Navigation';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { Register } from './components/Register/Register';
+import { Login } from './components/Login/Login';
 import { EditProfile } from './pages/EditProfile';
+
 function App() {
   return (
     <div className="min-h-screen bg-[#111827] text-white">
@@ -16,6 +18,7 @@ function App() {
             <Route path="/" element={<MainPage />} />
             <Route path="/budies/:id" element={<BuddyCardDetailed />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/edit/profile/:id" element={<EditProfile />} />
           </Routes>
         </AuthProvider>
