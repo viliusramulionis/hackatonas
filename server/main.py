485bfe823e2d4d9408bba592ac886bfcbe464982
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2025-06-07 19:57:24 +0300

    fixed main.py

--- server/main.py
+++ server/main.py
@@ -25,17 +25,3 @@ app.register_blueprint(interests, url_prefix='/api/interests')
 
 #languages priregistravimas
 app.register_blueprint(languages, url_prefix='/api/languages')
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-# Main update
-=======
-<<<<<<< HEAD
-
-
-# autentifikavimas
-=======
->>>>>>> 3eb94db0b58fce91cb6759633d463e6c7650cd6f
->>>>>>> ead3b8f9e635d69c4453c31dba74136ca7941611
->>>>>>> f9a0194b00945179daeaeb92b5498ef89e6a664f
